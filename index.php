ea454674e7f093e9a6d7af849ec9e2b10b9ebb30
ryanman313 <54728280+ryanman313@users.noreply.github.com>
2019-08-31 06:42:12 +0700

    file baru

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Transparant</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+	<body>
+		<div class="login-box">
+			<img src="unduhan.jpg" class="avatar">
+			<h1>Login Here</h1>
+			<form>
+				<p>Username</p>
+				<input type="text" name="Username" placeholder="Input Username">
+				<p>Password</p>
+				<input type="Password" name="Password" placeholder="Input Password">
+				<input type="submit" name="submit" value="Login">
+				<a href="home.html">Buat Akun</a></br>
+				<a href="#">Lupa Password</a>
+			</form>
+			
+		</div>
+
+	</body>
+</html>
